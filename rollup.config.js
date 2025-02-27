3a0b76aad858d6fb3cd49477083aa331d1b3681d
Joel Morrison <spinbitz@gmail.com>
2025-02-26 20:04:49 -0700

    cleanup

--- rollup.config.js
+++ rollup.config.js
@@ -2,37 +2,50 @@ import resolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
 import typescript from '@rollup/plugin-typescript';
 import { terser } from 'rollup-plugin-terser';
-import postcss from 'rollup-plugin-postcss';
+import external from 'rollup-plugin-peer-deps-external';
+import dts from 'rollup-plugin-dts';
 
-const isDev = process.env.NODE_ENV === 'development';
+const packageJson = require('./package.json');
 
-export default {
-  input: 'src/index.ts',
-  output: [
-    {
-      file: 'dist/index.js',
-      format: 'cjs',
-      sourcemap: true
-    },
-    {
-      file: 'dist/index.esm.js',
-      format: 'esm',
-      sourcemap: true
-    }
-  ],
-  external: ['react', 'react-dom', 'd3', 'recharts'],
-  plugins: [
-    resolve(),
-    commonjs(),
-    typescript({
-      tsconfig: './tsconfig.json',
-      declaration: true,
-      declarationDir: './dist/types'
-    }),
-    postcss({
-      modules: true,
-      minimize: !isDev
-    }),
-    !isDev && terser()
-  ]
-}; 
+export default [
+  {
+    input: 'src/index.ts',
+    output: [
+      {
+        file: packageJson.main,
+        format: 'cjs',
+        sourcemap: true,
+        name: 'fraop-mvi-dev-tools'
+      },
+      {
+        file: packageJson.module,
+        format: 'esm',
+        sourcemap: true
+      }
+    ],
+    plugins: [
+      external(),
+      resolve(),
+      commonjs(),
+      typescript({ tsconfig: './tsconfig.json' }),
+      terser()
+    ],
+    external: [
+      'react',
+      'react-dom',
+      '@mui/material',
+      '@mui/icons-material',
+      '@emotion/react',
+      '@emotion/styled',
+      'd3',
+      'recharts',
+      'rxjs'
+    ]
+  },
+  {
+    input: 'dist/types/index.d.ts',
+    output: [{ file: 'dist/index.d.ts', format: 'esm' }],
+    plugins: [dts()],
+    external: [/\.css$/]
+  }
+]; 
